05a55fa81682e05d0796dcf584a8b0f42b103552
Daniel Hopson <hdhopson88@yahoo.com>
2022-07-13 15:55:06 -0500

    Skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { component: HomePageComponent, path: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
